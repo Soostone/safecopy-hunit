d7dd64038ba1efb1682bcab31035674da3e4d5b1
Michael Xavier <michael@michaelxavier.net>
2017-03-31 16:04:31 -0700

    Init
    still need packaging, travis, readme, etc

---
+++ src/Test/HUnit/SafeCopy.hs
@@ -0,0 +1,135 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Test.HUnit.SafeCopy
+    ( testSafeCopy
+    , MissingFilesPolicy(..)
+    --TODO: drop exporrt
+    , SafeCopyFile(..)
+    ) where
+
+
+-------------------------------------------------------------------------------
+import           Control.Monad          (unless)
+import qualified Data.ByteString        as BS
+import           Data.List              (intercalate, stripPrefix, (\\))
+import qualified Data.SafeCopy          as SC
+import qualified Data.SafeCopy.Internal as SCI
+import qualified Data.Serialize         as S
+import qualified Path                   as P
+import qualified Path.Internal          as PI
+import qualified Path.IO                as PIO
+import qualified System.IO              as SIO
+import qualified Test.HUnit             as HU
+import           Text.Read              (readMaybe)
+-------------------------------------------------------------------------------
+
+
+-- | Safecopy provides a list of all supported versions. If we are
+-- unable to find a corresponding file for a version, what should we
+-- do? A missing file typically means you forgot to run tests when you
+-- were at one of the versions of @a@.
+data MissingFilesPolicy = IgnoreMissingFiles
+                        | WarnMissingFiles
+                        | FailMissingFiles
+                        deriving (Show, Eq)
+
+
+-------------------------------------------------------------------------------
+testSafeCopy
+    :: forall a b. (SC.SafeCopy a, Eq a, Show a)
+    => MissingFilesPolicy
+    -> P.Path b P.File
+    -- ^ Base filename, e.g. @test/data/MyType.golden@. Will be
+    -- postfixed with each version, e.g. @MyType.safecopy.1@,
+    -- @MyType.safecopy.2@, etc. If its a primitive value, versioning
+    -- is not supported and thus no extension will be added.
+    -> a
+    -- ^ The current value that all past versions of this file must upgrade to.
+    -> HU.Assertion
+testSafeCopy missingFilesPolicy baseFile a = do
+  case SC.objectProfile :: SC.Profile a of
+    SC.PrimitiveProfile                         -> do
+      -- dump file, test
+      assertLatest baseFile
+    SC.InvalidProfile e                         -> HU.assertFailure e
+    SC.Profile currentVersion supportedVersions -> do
+      let versions = (SCI.Version <$> supportedVersions) :: [SC.Version a]
+      let currentFile = mkVersionPath (SCI.Version currentVersion) baseFile
+      dumpVersionUnlessExists currentFile a
+      files <- discoverSafeCopyFiles baseFile
+      let missingVersions = versions \\ (scfVersion <$> files)
+      --TODO: make this a warning or omit based on option
+      unless (null missingVersions) $ do
+        let msg = ("Missing files for the following versions: " ++ intercalate "," (show . SCI.unVersion <$> missingVersions))
+        case missingFilesPolicy of
+          IgnoreMissingFiles -> return ()
+          WarnMissingFiles   -> SIO.hPutStrLn SIO.stderr msg
+          FailMissingFiles   -> HU.assertFailure msg
+      -- TODO: check versions
+      mapM_ (\f -> assertFile (scfPath f) a) files
+  where
+    assertLatest f = do
+      dumpVersionUnlessExists f a
+      assertFile f a
+
+
+
+-------------------------------------------------------------------------------
+data SafeCopyFile rel a = SafeCopyFile
+  { scfPath    :: P.Path rel P.File
+  , scfVersion :: SC.Version a
+  } deriving (Show)
+
+
+-------------------------------------------------------------------------------
+mkVersionPath :: SC.Version a -> P.Path rel P.File -> P.Path rel P.File
+mkVersionPath (SCI.Version v) (PI.Path fp) = PI.Path (fp ++ "." ++ show v)
+
+
+-------------------------------------------------------------------------------
+discoverSafeCopyFiles
+    :: P.Path rel P.File
+    -> IO [SafeCopyFile P.Abs a]
+discoverSafeCopyFiles baseFile = do
+  dir <- P.parent <$> PIO.makeAbsolute baseFile
+  (_, files) <- PIO.listDir dir
+  return [ SafeCopyFile f v | Just (f, v) <- check <$> files]
+  where
+    fname = P.toFilePath . P.filename
+    check f = do
+      ext <- stripPrefix (fname baseFile) (fname f)
+      v <- case ext of
+        '.':rawVersion -> SCI.Version <$> readMaybe rawVersion
+        _              -> Nothing
+      return (f, v)
+
+
+-------------------------------------------------------------------------------
+assertFile
+    :: ( SC.SafeCopy a
+       , Eq a
+       , Show a
+       )
+    => P.Path b P.File
+    -> a
+    -> HU.Assertion
+assertFile f expected = do
+  raw <- BS.readFile rawFile
+  case S.runGet SC.safeGet raw of
+    Left e       -> HU.assertFailure ("SafeCopy error in " ++ rawFile ++ ": " ++ e)
+    Right actual -> HU.assertEqual "" expected actual
+  where
+    rawFile = P.toFilePath f
+
+
+-------------------------------------------------------------------------------
+dumpVersionUnlessExists
+    :: (SC.SafeCopy a)
+    => P.Path rel P.File
+    -> a
+    -> IO ()
+dumpVersionUnlessExists f a = do
+ fabs <- PIO.makeAbsolute f
+ PIO.ensureDir (P.parent fabs)
+ exists <- PIO.doesFileExist f
+ unless exists $
+   BS.writeFile (P.toFilePath f) (S.runPut (SC.safePut a))
---
+++ test/Main.hs
@@ -0,0 +1,93 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies    #-}
+module Main
+    ( main
+    ) where
+
+
+-------------------------------------------------------------------------------
+import           Control.Exception
+import           Data.Foldable       (forM_)
+import           Data.List           (isPrefixOf)
+import           Data.SafeCopy
+import           Path
+import           Path.IO
+import qualified Test.HUnit.Lang     as HU
+import           Test.Tasty
+import           Test.Tasty.HUnit
+-------------------------------------------------------------------------------
+import           Test.HUnit.SafeCopy
+-------------------------------------------------------------------------------
+
+
+data TrivialRecord_v0 = TrivialRecord_v0
+                      deriving (Show, Eq)
+
+
+data TrivialRecord_v1 = TrivialRecord_v1 Int
+                      deriving (Show, Eq)
+
+
+data TrivialRecord = TrivialRecord Int String
+                   deriving (Show, Eq)
+
+testFilesDir :: Path Rel Dir
+testFilesDir = $(mkRelDir "test/files")
+
+trivialRecordBase :: Path Rel File
+trivialRecordBase = testFilesDir </> $(mkRelFile "TrivialRecord")
+
+
+$(deriveSafeCopy 0 'base ''TrivialRecord_v0)
+$(deriveSafeCopy 1 'extension ''TrivialRecord_v1)
+$(deriveSafeCopy 2 'extension ''TrivialRecord)
+
+
+instance Migrate TrivialRecord_v1 where
+  type MigrateFrom TrivialRecord_v1 = TrivialRecord_v0
+  migrate TrivialRecord_v0 = TrivialRecord_v1 42
+
+
+instance Migrate TrivialRecord where
+  type MigrateFrom TrivialRecord = TrivialRecord_v1
+  migrate (TrivialRecord_v1 n) = TrivialRecord n "example"
+
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "safecopy-hunit"
+  [ testCase "tests multiple versions" $
+      testSafeCopy FailMissingFiles trivialRecordBase trivialRecord
+  , testCase "can ignore missing versions" $ withVersionGaps $ \tmpDir ->
+      testSafeCopy IgnoreMissingFiles (tmpDir </> $(mkRelFile "TrivialRecord")) trivialRecord
+  , testCase "can fail on missing versions" $ withVersionGaps $ \tmpDir -> do
+      res <- try (testSafeCopy FailMissingFiles (tmpDir </> $(mkRelFile "TrivialRecord")) trivialRecord)
+      let expectedMsg = "Missing files for the following versions: 1"
+      case res of
+        Left (HU.HUnitFailure _ (HU.Reason actualMsg)) -> assertEqual "" expectedMsg actualMsg
+        _ -> assertFailure ("Expected failure reason of \"" ++ expectedMsg ++ "\" but got " ++ show res)
+  , testCase "can fail on safecopy errors" $ withTempFiles [
+        $(mkRelFile "TrivialRecord.0")
+      , $(mkRelFile "TrivialRecord.1")
+      ] $ \tmpDir -> do
+      let badFile = (tmpDir </> $(mkRelFile "TrivialRecord.2"))
+      writeFile (toFilePath badFile) "corrupted"
+      res <- try (testSafeCopy FailMissingFiles (tmpDir </> $(mkRelFile "TrivialRecord")) trivialRecord)
+      let pfx = "SafeCopy error in " ++ toFilePath badFile ++ ":"
+      case res of
+        Left (HU.HUnitFailure _ (HU.Reason actualMsg))
+          | pfx `isPrefixOf` actualMsg -> return ()
+        _ -> assertFailure ("Expected failure starting with \"" ++ pfx ++ "\" but got " ++ show res)
+  ]
+  where
+    trivialRecord = TrivialRecord 42 "example"
+    withTempFiles files callback = withSystemTempDir "safecopy-hunit-tests" $ \tmpDir -> do
+      forM_ files $ \f ->
+        copyFile (testFilesDir </> f) (tmpDir </> f)
+      callback tmpDir
+    withVersionGaps = withTempFiles
+      [ $(mkRelFile "TrivialRecord.0")
+      , $(mkRelFile "TrivialRecord.2")
+      ]
